63177cd57f29d5f0715c35a9d38eda9233a4115f
Rob Hoes <rob.hoes@citrix.com>
2025-02-07 14:24:29 +0000

    h2-unix

---
+++ ocaml/libs/http-lib/httpun-unix/h2_unix.ml
@@ -0,0 +1,17 @@
+module D = Debug.Make (struct let name = "gluten_unix" end)
+
+open D
+
+module Server = struct
+  let create_connection_handler ?(config = H2.Config.default) ~request_handler
+      ~error_handler client_addr socket =
+    let create_connection =
+      H2.Server_connection.create ~config
+        ~error_handler:(error_handler client_addr)
+    in
+    debug "H2_unix.Server.create_connection_handler" ;
+    Gluten_unix.Server.create_upgradable_connection_handler
+      ~read_buffer_size:config.read_buffer_size
+      ~protocol:(module H2.Server_connection)
+      ~create_protocol:create_connection ~request_handler client_addr socket
+end
